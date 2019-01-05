3f20dbc5903c62547b7287b428570be2f482e1c2
Tyler Hunter <tyler.a.hunter@gmail.com>
2019-01-04 23:52:57 -0500

    adding shopify javascript sdk

--- app.js
+++ app.js
@@ -1,50 +1,94 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+// var express = require('express');
+// var path = require('path');
+// var favicon = require('serve-favicon');
+// var logger = require('morgan');
+// var cookieParser = require('cookie-parser');
+// var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+// var index = require('./routes/index');
+// var users = require('./routes/users');
 
-var shopify = require('./routes/shopify');
+// var shopify = require('./routes/shopify');
 
-require('dotenv').config();
-var app = express();
+// require('dotenv').config();
+// var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// // uncomment after placing your favicon in /public
+// //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
-app.use('/shopify', shopify);
+// app.use('/', index);
+// app.use('/users', users);
+// app.use('/shopify', shopify);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
+// // error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+// module.exports = app;
+
+import Client from 'shopify-buy';
+
+const client = Client.buildClient({
+  domain: 'your-shop-name.myshopify.com',
+  storefrontAccessToken: 'your-storefront-access-token'
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// Fetch all products in your shop
+client.product.fetchAll().then((products) => {
+  // Do something with the products
+  console.log(products);
 });
 
-module.exports = app;
+// Fetch a single product by ID
+const productId = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0Lzc4NTc5ODkzODQ=';
+
+client.product.fetch(productId).then((product) => {
+  // Do something with the product
+  console.log(product);
+});
+
+// Fetch all collections, including their products
+client.collection.fetchAllWithProducts().then((collections) => {
+  // Do something with the collections
+  console.log(collections);
+  console.log(collections[0].products);
+});
+
+// Fetch a single collection by ID, including its products
+const collectionId = 'Z2lkOi8vc2hvcGlmeS9Db2xsZWN0aW9uLzM2OTMxMjU4NA==';
+
+client.collection.fetchWithProducts(collectionId).then((collection) => {
+  // Do something with the collection
+  console.log(collection);
+  console.log(collection.products);
+});
+
+// Create an empty checkout
+client.checkout.create().then((checkout) => {
+  // Do something with the checkout
+  console.log(checkout);
+});
+
